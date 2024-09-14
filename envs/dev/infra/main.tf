98da72c18168c088af75771c1cff34601adda723
Jazeel <jazeel@sph.com.sg>
2024-09-14 11:50:45 +0800

    fix typo

--- envs/dev/infra/main.tf
+++ envs/dev/infra/main.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "name" {
   bucket = "jaz-hcp-terraform-bucket-dev" #
 }
 
-resource "aws_s3_bucket" "name" {
+resource "aws_s3_bucket" "name2" {
   bucket = "jaz-hcp-terraform-bucket-dev-2" #add second bucket
 }

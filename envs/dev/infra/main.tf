d21f30d6172e4255b3c260326eb15a44854595d3
Jazeel <jazeel@sph.com.sg>
2024-09-14 11:50:19 +0800

    Add second bucket to test cost estimare

--- envs/dev/infra/main.tf
+++ envs/dev/infra/main.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "name" {
   bucket = "jaz-hcp-terraform-bucket-dev" #
 }
+
+resource "aws_s3_bucket" "name" {
+  bucket = "jaz-hcp-terraform-bucket-dev-2" #add second bucket
+}
